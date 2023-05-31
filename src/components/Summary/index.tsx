adb933e241af52aaa5a2c332d4793c52393b53d9
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-05-31 14:42:51 -0300

    feat: Display dynamic income and expense balances in Summary component
    This commit introduces a new feature in the Summary component, where the income and expense balances are now dynamically displayed based on the data retrieved from our fake API created with Mockoon.
    
    Implemented a method to format the values in the Brazilian Real currency format.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,38 @@
+import { useStore } from "effector-react";
+
+import TransactionStore from "../../stores/TransactionStore/TransactionStore";
+
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from "phosphor-react";
 
 import { SummaryCard, SummaryContainer } from "./styles";
 import { defaultTheme } from "../../styles/defaultTheme";
 
 export const Summary = () => {
+  const { transactions } = useStore(TransactionStore);
+
+  const formatMoney = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  const accountBalance = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 0) {
+        acc.incomes += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.outcomes += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+      return acc;
+    },
+    {
+      incomes: 0,
+      outcomes: 0,
+      total: 0,
+    }
+  );
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -12,7 +41,7 @@ export const Summary = () => {
           <ArrowCircleUp size={32} color={defaultTheme["yellow-500"]} />
         </header>
 
-        <strong>R$ 17.500,00</strong>
+        <strong>{formatMoney.format(accountBalance.incomes)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -21,7 +50,7 @@ export const Summary = () => {
           <ArrowCircleDown size={32} color={defaultTheme["red-500"]} />
         </header>
 
-        <strong>R$ 500,00</strong>
+        <strong>{formatMoney.format(accountBalance.outcomes)}</strong>
       </SummaryCard>
 
       <SummaryCard variant="balance">
@@ -30,7 +59,7 @@ export const Summary = () => {
           <CurrencyDollar size={32} color={defaultTheme["yellow-500"]} />
         </header>
 
-        <strong>R$ 17.000,00</strong>
+        <strong>{formatMoney.format(accountBalance.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
